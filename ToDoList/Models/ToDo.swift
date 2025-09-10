94a32a9a6411b88d152af7ad96e9375de618c542
ronlemire2 <ronlemire2@gmail.com>
2025-09-10 15:44:40 -0700

    Moved SortedOptjon and SortedToDoList to their own file

--- ToDoList/Models/ToDo.swift
+++ ToDoList/Models/ToDo.swift
@@ -31,7 +31,7 @@ extension ToDo {
         let container = try! ModelContainer(for: ToDo.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
         
         // Add mock data
-        container.mainContext.insert(ToDo(item: "Create SwiftData Lessons", reminderIsOn: true, dueDate: Date.now + 60*60*24, notes: "Now with iOS 16 & Xcode 18", isCompleted: false))
+        container.mainContext.insert(ToDo(item: "Create SwiftData Lessons", reminderIsOn: true, dueDate: Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!, notes: "Now with iOS 16 & Xcode 18", isCompleted: false))
         container.mainContext.insert(ToDo(item: "Montenegrin Educators Talk", reminderIsOn: true, dueDate: Date.now + 60*60*44, notes: "They want to learn about entrepreneurship", isCompleted: false))
         container.mainContext.insert(ToDo(item: "Post Flyers for Swift in Santiago", reminderIsOn: true, dueDate: Date.now + 60*60*72, notes: "To be held at UAH in Chile", isCompleted: false))
         container.mainContext.insert(ToDo(item: "Prepare old iPhone for Lily", reminderIsOn: true, dueDate: Date.now + 60*60*12, notes: "She gets my old pro", isCompleted: false))
@@ -39,6 +39,4 @@ extension ToDo {
         return container
     }
 }
-/*
- var dueDate = Calendar.current.date(byAdding: .day, value: 1, to: Date.now)!
- */
+
--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -20,9 +20,9 @@ struct ToDoListApp: App {
     // Will allow us to find where our simulator data is saved:
     init() {
         print(URL.applicationSupportDirectory.path(percentEncoded: false))
+        /*
+        /Users/imac/Library/Developer/CoreSimulator/Devices/344480B8-6E37-4953-ABBF-E0AFD6FC98A0/data/Containers/Data/Application/B06CC1F4-0812-4D72-BF12-2247A2CF95C2/Library/Application Support/
+        */
     }
-    
-    /*
-    /Users/imac/Library/Developer/CoreSimulator/Devices/344480B8-6E37-4953-ABBF-E0AFD6FC98A0/data/Containers/Data/Application/B06CC1F4-0812-4D72-BF12-2247A2CF95C2/Library/Application Support/
-    */
+
 }
