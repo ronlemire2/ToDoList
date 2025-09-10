7bdb9efebcca59c8614d3fef6a5ec993e133cb77
ronlemire2 <ronlemire2@gmail.com>
2025-09-10 09:27:23 -0700

    Ch 5.8 Interactive @Query in SwiftData - Change @Query Properties with a Picker

--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -8,75 +8,83 @@
 import SwiftUI
 import SwiftData
 
-struct ToDoListView: View {
+enum SortOption: String, CaseIterable {
+    case asEntered = "Unsorted"
+    case alphabetical = "A-Z"
+    case reversed = "Z-A"
+    case chronological = "Date"
+    case completed = "Not Done"
+}
+
+struct SortedToDoList: View {
     @Query var toDos: [ToDo]
-    @State private var sheetIsPresented = false
     @Environment(\.modelContext) var modelContext
+    let sortSelection: SortOption
+    
+    init(sortSelection: SortOption) {
+        self.sortSelection = sortSelection
+        switch self.sortSelection {
+        case .asEntered:
+            _toDos = Query()
+        case .alphabetical:
+            _toDos = Query(sort: \.item, order: .forward)
+        case .reversed:
+            _toDos = Query(sort: \.item, order: .reverse)
+        case .chronological:
+            _toDos = Query(sort: \.dueDate)
+        case .completed:
+            _toDos = Query(filter: #Predicate {$0.isCompleted == false})
+        }
+    }
+    
+    var body: some View {
+        List {
+            ForEach(toDos) { toDo in
+                VStack (alignment: .leading) {
+                    HStack {
+                        Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
+                            .onTapGesture {
+                                toDo.isCompleted.toggle()
+                            }
+                        NavigationLink {
+                            DetailView(toDo: toDo)
+                        } label: {
+                            Text(toDo.item)
+                        }
+                        .swipeActions {
+                            Button("Delete", role: .destructive) {
+                                modelContext.delete(toDo)
+                            }
+                        }
+                    }
+                    .font(.title3)
+                    
+                    HStack {
+                        Text(toDo.dueDate.formatted(date: .abbreviated, time: .shortened))
+                            .foregroundStyle(.secondary)
+                        
+                        if toDo.reminderIsOn {
+                            Image(systemName: "calendar.badge.clock")
+                                .symbolRenderingMode(.multicolor)
+                        }
+                    }
+                }
+
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ToDoListView: View {
+    @State private var sheetIsPresented = false
+    @State private var sortSelection: SortOption = .asEntered
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(toDos) { toDo in
-                    VStack (alignment: .leading) {
-                        HStack {
-                            Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
-                                .onTapGesture {
-                                    toDo.isCompleted.toggle()
-                                    // Save after .toggle wasn't working in video so needed these lines.
-                                    // Save after .toggle was working for me so don't need these lines.
-                                    /*
-                                    guard let _ = try? modelContext.save() else {
-                                        print("😡 ERROR: Save after .toggle on ToDoListView did not work.")
-                                        return
-                                    }
-                                    */
-                                }
-                            NavigationLink {
-                                DetailView(toDo: toDo)
-                            } label: {
-                                Text(toDo.item)
-                            }
-                            .swipeActions {
-                                Button("Delete", role: .destructive) {
-                                    modelContext.delete(toDo)
-                                    // Delete wasn't working in video so needed these lines.
-                                    // Delete was working for me so don't need these lines.
-                                    /*
-                                    guard let _ = try? modelContext.save() else {
-                                        print("😡 ERROR: Save after .delete on ToDoListView did not work.")
-                                        return
-                                    }
-                                    */
-                                }
-                            }
-                        }
-                        .font(.title3)
-                        
-                        HStack {
-                            Text(toDo.dueDate.formatted(date: .abbreviated, time: .shortened))
-                                .foregroundStyle(.secondary)
-                            
-                            if toDo.reminderIsOn {
-                                Image(systemName: "calendar.badge.clock")
-                                    .symbolRenderingMode(.multicolor)
-                            }
-                        }
-                    }
-
-                }
-                /* Alternative to .swipeActions
-                .onDelete { indexSet in
-                    indexSet.forEach({modelContext.delete(toDos[$0])})
-                    guard let _ = try? modelContext.save() else {
-                        print("😡 ERROR: Save after .delete on ToDoListView did not work.")
-                        return
-                    }
-                }
-                */
-            }
+            SortedToDoList(sortSelection: sortSelection)
             .navigationTitle("To Do List")
             .navigationBarTitleDisplayMode(.automatic)
-            .listStyle(.plain)
             .sheet(isPresented: $sheetIsPresented) {
                 NavigationStack {
                     DetailView(toDo: ToDo())
@@ -89,6 +97,14 @@ struct ToDoListView: View {
                     } label: {
                         Image(systemName: "plus")
                     }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Picker("", selection: $sortSelection) {
+                        ForEach(SortOption.allCases, id: \.self) { sortOrder in
+                            Text(sortOrder.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
             }
         }
